651624b44fd3e74fa641ae4788fa54d5f1776021
Daniel Stutzman <dtstutz@gmail.com>
2012-10-31 15:01:39 -0600

    Use youtube-dl and ffmpeg to convert Youtube to mp3 instead of relying on an external service.

--- youtube_to_mp3.sh
+++ youtube_to_mp3.sh
@@ -1,34 +1,18 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [ "$1" == "" ]; then
-  echo "You must specify the Youtube ID as the first parameter (for example, $0 ALf5wpTokKA > ALf5wpTokKA.mp3)" 1>&2
+  echo "You must specify the Youtube ID as the first parameter (for example, $0 ALf5wpTokKA)" 1>&2
   exit 1
 fi
 VIDEO_ID="$1"
 
-#VIDEO_ID='XV7DOBFj-KI' # Dutty Love
-#VIDEO_ID='SMM1WhmlQzI' # Hasta Abajo
-#VIDEO_ID='ALf5wpTokKA' # shortest video on youtube
+python tools/youtube-dl.py "http://www.youtube.com/watch?v=$VIDEO_ID"
 
-OUT=`curl -s --data "url=http://www.youtube.com/watch?v=$VIDEO_ID" "http://www.makeitmp3.com/check.php"`
-#OUT="WAIT|24|XV7DOBFj-KI|YouTube|qc|./view/YouTube/XV7DOBFj-KI/3d5c8dd544bda97a0f70e50933cc3934/|"
-#echo $OUT
+# -vn ignores the video, just looks at audio
+# -acodec copy keeps the same audio codec
+#ffmpeg -i "$VIDEO_ID.flv" -vn -acodec copy "$VIDEO_ID.mp4"
+ffmpeg -i "$VIDEO_ID.flv" -vn -acodec mp3 "$VIDEO_ID.mp3"
 
-RELATIVE_URL=`echo $OUT | awk "-F|" '{print $6}'`
-#RELATIVE_URL='./view/YouTube/XV7DOBFj-KI/3d5c8dd544bda97a0f70e50933cc3934/'
-#echo $RELATIVE_URL
+if [ "$?" != "0" ]; then exit 1; fi
 
-VIEW_URL=`echo $RELATIVE_URL | sed 's/\./http:\/\/www.makeitmp3.com/'`
-#VIEW_URL="http://www.makeitmp3.com/view/YouTube/XV7DOBFj-KI/3d5c8dd544bda97a0f70e50933cc3934/"
-#echo $VIEW_URL
-
-LINK_HTML=`curl -s "$VIEW_URL" | grep '<a class="download" href=' | tail -1`
-#LINK_HTML='<a class="download" href="http://www.makeitmp3.com/load/YouTube/XV7DOBFj-KI/3073280d39d4ad8ed7809ed2cbd98bab/">Don Omar Ft. Natti Natasha - Dutty Love [Official Version] ???NEW ?? 2011???</a>'
-#echo $LINK_HTML
-
-LOAD_URL=`echo "$LINK_HTML" | sed 's/.*href="\([^"]*\)".*/\1/'`
-#LOAD_URL="http://www.makeitmp3.com/load/YouTube/XV7DOBFj-KI/3073280d39d4ad8ed7809ed2cbd98bab/"
-#echo $LOAD_URL
-
-#curl -D 302.txt $LOAD_URL
-curl -s -L $LOAD_URL
+rm "$VIDEO_ID.flv"
