82940a521267df4df2e24095aee63b20e7c261e1
Daniel Stutzman <dtstutz@gmail.com>
2012-10-31 22:28:30 -0600

    Started back-end search.

--- backend/backend_app.rb
+++ backend/backend_app.rb
@@ -1,8 +1,21 @@
 require 'rubygems' if RUBY_VERSION < '1.9'
 require 'sinatra/base'
 
+QUERY_REGEX = /^\w+$/
+
 class BackendApp < Sinatra::Base
   get '/' do
-    'Minimal Sinatra Hello World!'
+    redirect '/search'
+  end
+
+  get '/search' do
+    haml :search
+  end
+
+  post '/search' do
+    query = params['query']
+    raise "Query doesn't pass white list" if !query.match(QUERY_REGEX)
+    @results = `grep -ri "#{query}" ../song_lyrics`
+    haml :search
   end
 end
