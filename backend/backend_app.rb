6fa4095bd964d6fe4178ce657faa638baf565468
Daniel Stutzman <dtstutz@gmail.com>
2012-11-14 10:17:18 -0700

    Show terms and counts on search page.

--- backend/backend_app.rb
+++ backend/backend_app.rb
@@ -6,24 +6,41 @@ require './model'
 QUERY_REGEX = /^\w+$/
 
 class BackendApp < Sinatra::Base
+  def get_term_counts
+    reader = Ferret::Index::IndexReader.new('index')
+    @term_counts = []
+    reader.terms(:lyrics).each do |term, doc_freq|
+      @term_counts << [term, doc_freq]
+    end
+  end
+
+  def serve_search
+    query = params['query']
+    raise "Query doesn't pass white list" if query && !query.match(QUERY_REGEX)
+
+    if query
+      @results = []
+      with_ferret_index do |index|
+        index.search_each("lyrics:#{query}") do |id, score|
+          doc = index[id]
+          @results << "song_id='#{doc[:song_id]}' lyrics='#{doc[:lyrics]}'"
+        end
+      end
+    end
+
+    get_term_counts
+    haml :search
+  end
+
   get '/' do
     redirect '/search'
   end
 
   get '/search' do
-    haml :search
+    serve_search
   end
 
   post '/search' do
-    query = params['query']
-    raise "Query doesn't pass white list" if !query.match(QUERY_REGEX)
-    @results = []
-    with_ferret_index do |index|
-      index.search_each("lyrics:#{query}") do |id, score|
-        doc = index[id]
-        @results << "song_id='#{doc[:song_id]}' lyrics='#{doc[:lyrics]}'"
-      end
-    end
-    haml :search
+    serve_search
   end
 end
