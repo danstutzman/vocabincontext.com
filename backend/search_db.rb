191dd7b0a8a55bfbd7d7b210a97dcfac572ff40c
Daniel Stutzman <dtstutz@gmail.com>
2012-11-14 12:43:48 -0700

    now showing the positions of all the terms

--- backend/search_db.rb
+++ backend/search_db.rb
@@ -5,8 +5,15 @@ term = ARGV[0] or raise "Specify term as first argument"
 with_ferret_index do |index|
   query = "lyrics:#{term}"
   index.search_each(query) do |id, score|
-    p index.term_vector(id, :lyrics)
+    term_vector = index.term_vector(id, :lyrics)
+    term_vector.terms.each do |term|
+      p term.text
+      p term.positions.map { |position|
+        term_vector.offsets[position].start..term_vector.offsets[position].end
+      }
+    end
+
     doc = index[id]
-    puts "song_id='#{doc[:song_id]}' lyrics='#{doc[:lyrics]}'"
+    puts "- song_id='#{doc[:song_id]}' lyrics='#{doc[:lyrics]}'"
   end
 end
