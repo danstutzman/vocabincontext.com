1af26e63dbb067fa7a02a32ca98752bde14bb52d
Daniel Stutzman <dtstutz@gmail.com>
2012-11-22 19:53:04 -0700

    Rip out UiSpec and remove duplicate listing of specs to run.

--- backend/public/js/main_with_tests.js
+++ backend/public/js/main_with_tests.js
@@ -1,6 +1,7 @@
 define(['cs!app/app',
         "cs!spec/SoundGridSpec",
         "cs!spec/TimeSeriesSpec",
-        "cs!spec/UiSpec",
         "cs!spec/LyricsTableDataSpec"
-],function(app){});
+],function(app){
+  return app;
+});
