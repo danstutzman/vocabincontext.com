5df5daa6bcb4c31fdc5241be0835280d11a76434
Daniel Stutzman <dtstutz@gmail.com>
2012-11-28 09:52:39 -0700

    Avoid warning message when running serve_backend on Ubuntu.

--- tools/serve_backend.sh
+++ tools/serve_backend.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 cd `dirname $0`/..
 
-# Mac
-open "http://localhost:9393"
-
-# Ubuntu
-google-chrome "http://localhost:9393" >/dev/null 2>/dev/null &
+if [ -e /usr/bin/google-chrome ]; then
+  # Ubuntu
+   /usr/bin/google-chrome "http://localhost:9393" >/dev/null 2>/dev/null &
+else
+  # Mac
+  open "http://localhost:9393"
+fi
 
 cd backend
 ENV="$ENV" bundle exec rerun -- rackup --port 9393 config.ru
